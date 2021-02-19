c32988ae39ff381d9541852d6cdbfb5f81c57db2
Alyzsa <48876765+alyzsat@users.noreply.github.com>
2021-02-18 22:27:23 -0800

    Create separate pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import SignIn from './components/Registration/SignIn'
-import SignUp from './components/Registration/SignUp'
-import Home from './components/Home/Home'
+import SignIn from './components/Registration/SignIn';
+import SignUp from './components/Registration/SignUp';
+import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Profile from "./components/Profile";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      {/* Insert routing here */} 
-      {/*putting home here for now*/}
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div class="App">
+        <Navigation/>
+        <br/><br/><br/><br/><br/><br/><br/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/search" component={Search}/>
+        <Route path="/profile" component={Profile}/>
+      </div>
+    </BrowserRouter>
   )
 }
 
