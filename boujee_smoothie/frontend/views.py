ef768e0561b055700bd8098d65525663bc24edae
Alyzsa <48876765+alyzsat@users.noreply.github.com>
2021-02-19 02:18:13 -0800

    Revert "Merge pull request #2 from cedricll/django"
    This reverts commit e0c6f38114b44ce7507e5a0ac616615c76606278, reversing
    changes made to 4eaf65e0a9751828c20ce60a81e49e2bae0dd334.

--- boujee_smoothie/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request, *args, **kwargs):
-    ''' render index template, and react will render 
-        things inside of this
-    '''
-    return render(request, "frontend/index.html") 
