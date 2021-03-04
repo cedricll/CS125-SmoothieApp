4cf28c5e254bfc7582f83a883d5df5526476c84a
Cedric Lim <cedriclim99@yahoo.com>
2021-03-04 05:12:22 -0800

    search implmented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,71 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../App.css';
 import Navigation from "./Navigation";
+import Recipe from "./Recipe";
 
-class Search extends Component {
+const Search = () => {
+    const APP_ID = 'bb6b3d95';
+    const APP_KEY = 'c79a906105c5e348ec12bb9b47b0b363';
+    const api = `https://api.edamam.com/search?q=smoothie&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-    render() {
-        return (
-            <div className="App">
-                <Navigation/>
-                <br/><br/><br/><br/><br/><br/><br/>
-                <h1>Search page</h1>
-            </div>
-        )
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('strawberry');
+
+    useEffect( () => {
+        console.log("useEffect has run");
+        getRecipes();
+    }, [query]);
+
+    const getRecipes = async () => { // this is the search query for smoothies only
+        // const response = await fetch( // loop through "search" to create a new query string
+        // `https://api.edamam.com/search?q=smoothie&${queryDiet}app_id=${APP_ID}&app_key=${APP_KEY}`
+        // );
+        const response = await fetch( // loop through "search" to create a new query string
+        `https://api.edamam.com/search?q=smoothie ${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
+        const data = await response.json();
+        setRecipes(data.hits);
+        console.log(data);
     }
+
+    const updateSearch = (e) => {
+        setSearch(e.target.value);
+        console.log(search);
+    }   
+
+    const getSearch = (e) => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
+
+    return (
+        <div className="App">
+            <Navigation/>
+            <br/><br/><br/><br/><br/><br/><br/>
+            <h1>Smoothie Search</h1>
+            <form onSubmit={getSearch} className="search-form">
+                <input className="" type="text" value={search} onChange={updateSearch}/>
+                <button className="Button" type="submit">
+                    Search
+                </button>
+            </form>
+            <br/><br/><br/>
+            {recipes.map(recipe => (
+            <Recipe key={recipe.recipe.label} 
+                    title={recipe.recipe.label} 
+                    source={recipe.recipe.source}
+                    link={recipe.recipe.url}
+                    image={recipe.recipe.image}
+                    healthLabels={recipe.recipe.healthLabels}
+                    dietLabels={recipe.recipe.dietLabels}
+            />
+            ))}
+
+        </div>
+    )
+    
 }
 
 export default Search
